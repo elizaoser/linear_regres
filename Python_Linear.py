6bd2b477032047baf2500251769baea61cbc694e
Eliza Oser <oser.18@osu.edu>
2023-07-11 14:14:50 +0000

    Python Linear Regression Files and PNGs

--- Python_Linear.py
+++ Python_Linear.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Python Linear Regression Model
+# Python Linear Regression Model
 
-# # Import pandas, sklearn.linear_model, matplotlib.pyplot
+# Import pandas, sklearn.linear_model, matplotlib.pyplot
+
+import matplotlib.pyplot as plt
+import os
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
 import sys
-import os
 
 print("Running linear modelling of data python script")
 print()
@@ -25,32 +26,33 @@ base,ext = os.path.splitext(filename)
 print("Loading dataset {}".format(filename))
 print()
 
-# # Use the read_csv() function
+
+# Use the read_csv() function
 dataset = pd.read_csv(filename)
 dataset.describe
-dataset
+print(dataset)
 
+# Plot Data
 plt.scatter(dataset[['x']], dataset[['y']], color = 'red')
-plt.title('y vs x')
+plt.title("y vs x for {}".format(base))
 plt.xlabel('x')
 plt.ylabel('y')
+plt.savefig("{}.png".format(base))
 
-# # Adjusted R-squared
-model.score(dataset[['x']], dataset[['y']])
-
-
-# # Fitting Linear Regression to the Dataset
+# Fitting Linear Regression to the Dataset
 model = LinearRegression()
 model.fit(dataset[['x']], dataset[['y']])
 
+# Adjusted R-squared
+model.score(dataset[['x']], dataset[['y']])
 
-# # Visualizing the Linear Regression results
+
+# Visualizing the Linear Regression results
 plt.scatter(dataset[['x']], dataset[['y']], color = 'red')
 plt.plot(dataset[['x']], model.predict(dataset[['x']]), color = 'blue')
-plt.title('y vs x')
+plt.title("model of y vs x for {}".format(base))
 plt.xlabel('x')
 plt.ylabel('y')
-plt.show()
-
+plt.savefig("{}_model.png".format(base))
 
 
